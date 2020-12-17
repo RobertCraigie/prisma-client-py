5818be7e3e7af17368bfafae90e3214e7fdd24b3
Robert Craigie <robertcraigie30@gmail.com>
2020-12-17 03:23:53 +0000

    feat(logging): add debug logging setup

--- prisma/cli/cli.py
+++ prisma/cli/cli.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
+import logging
+
 from prisma import generator
 
 
@@ -8,6 +11,10 @@ __all__ = ('main',)
 
 
 def main():
+    if os.environ.get('PRISMA_PY_DEBUG'):
+        logging.basicConfig()
+        logging.root.setLevel(logging.DEBUG)
+
     args = sys.argv
     if len(args) > 1 and args[1] == 'generate':
         generator.run()
