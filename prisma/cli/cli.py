f8c14e77bc89d10d420a7e5cfb2eb0f87b2073f3
Robert Craigie <robertcraigie30@gmail.com>
2020-12-25 03:32:37 +0000

    feat(jsonrpc): add jsonrpc
    this is used by prisma for generating the client

--- prisma/cli/cli.py
+++ prisma/cli/cli.py
@@ -3,23 +3,94 @@
 import os
 import sys
 import logging
+import contextlib
 
-from prisma import generator
+from .. import generator, jsonrpc
 
 
 __all__ = ('main',)
 
+log = logging.getLogger(__name__)
+
 
 def main():
-    if os.environ.get('PRISMA_PY_DEBUG'):
-        logging.basicConfig()
-        logging.root.setLevel(logging.DEBUG)
+    with setup_logging():
+        args = sys.argv
+        if len(args) > 1:
+            print('Prisma CLI has not been implemented yet.', file=sys.stderr)
+            sys.exit(1)
 
-    args = sys.argv
-    if len(args) > 1 and args[1] == 'generate':
-        generator.run(args[2:])
-    else:
-        print('Prisma CLI has not been implemented yet.')
+        if not os.environ.get('PRISMA_GENERATOR_INVOCATION'):
+            log.warning(
+                'This command is only meant to be invoked internally. Please run the following instead:'
+            )
+            log.warning('`python3 -m prisma <command>`')
+            log.warning('e.g.')
+            log.warning('python3 -m prisma generate')
+            sys.exit(1)
+
+        invoke_prisma()
+
+
+@contextlib.contextmanager
+def setup_logging():
+    try:
+        logger = logging.getLogger()
+
+        # TODO: this does not log anything when running from the prisma cli
+        if os.environ.get('PRISMA_PY_DEBUG'):
+            logger.setLevel(logging.DEBUG)
+        else:
+            logger.setLevel(logging.INFO)
+
+        fmt = logging.Formatter(
+            '[{levelname:<7}] {name}: {message}',
+            style='{',
+        )
+        handler = logging.StreamHandler()
+        handler.setFormatter(fmt)
+        logger.addHandler(handler)
+        logger.addHandler(logging.StreamHandler())
+
+        yield
+    finally:
+        handler.close()
+        logger.removeHandler(handler)
+
+
+def invoke_prisma():
+    while True:
+        try:
+            line = jsonrpc.readline()
+        except TimeoutError:
+            log.debug('Timed out waiting for stdin input')
+            return
+
+        request = jsonrpc.parse(line)
+        log.debug(
+            'Received request method: %s with params: %s',
+            request.method,
+            request.params,
+        )
+
+        response = None
+        if request.method == 'getManifest':
+            response = jsonrpc.Response(
+                id=request.id,
+                result=dict(
+                    manifest=jsonrpc.Manifest(
+                        defaultOutput=str(generator.BASE_PACKAGE_DIR.absolute()),
+                        prettyName='Prisma Client Py',
+                    )
+                ),
+            )
+        elif request.method == 'generate':
+            raise NotImplementedError('Client generation has not been implemented yet')
+        else:
+            raise RuntimeError(f'JSON RPC received unexpected method: {request.method}')
+
+        if response is not None:
+            jsonrpc.reply(response)
 
 
 if __name__ == '__main__':
---
+++ prisma/jsonrpc.py
@@ -0,0 +1,78 @@
+import sys
+import json
+import signal
+import logging
+from typing import Dict, List, Optional
+from pydantic import BaseModel
+
+
+log = logging.getLogger(__name__)
+
+
+class Request(BaseModel):
+    # JSON RPC protocol version
+    jsonrpc: str = '2.0'
+
+    # identifies a request
+    id: int
+
+    # request intention
+    method: str
+
+    # request payload
+    params: Optional[Dict]
+
+
+class Response(BaseModel):
+    id: int
+    jsonrpc: str = '2.0'
+    result: Optional[Dict]
+
+
+class Manifest(BaseModel):
+    prettyName: str
+    defaultOutput: str
+    denylist: Optional[List[str]]
+    requiresEngines: Optional[List[str]]
+    requiresGenerators: Optional[List[str]]
+
+
+# TODO: proper types
+method_mapping = {
+    'getManifest': Request,
+    'generate': Request,
+}
+
+
+def readline() -> str:
+    def handler(signum, frame):
+        raise TimeoutError("Timed out waiting for stdin")
+
+    # TODO: windows compatibility
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(3)
+
+    line = input()
+    log.debug('Received %s', line)
+    return line
+
+
+def parse(line) -> Request:
+    data = json.loads(line)
+    try:
+        method = data['method']
+    except (KeyError, TypeError):
+        # TODO
+        raise
+    else:
+        request_type = method_mapping.get(method)
+        if request_type is None:
+            raise RuntimeError(f'Unknown method: {method}')
+
+    return request_type(**data)
+
+
+def reply(response: Response) -> None:
+    dumped = json.dumps(response.dict()) + '\n'
+    print(dumped, file=sys.stderr, flush=True)
+    log.debug('Replied with %s', dumped)
