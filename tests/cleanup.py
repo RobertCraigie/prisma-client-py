3162ec6e6c08b852b1df886e694a164b5ee861b8
Robert Craigie <robertcraigie30@gmail.com>
2020-12-27 23:14:44 +0000

    refactor(tests): generate the client to the cwd and cleanup after

---
+++ tests/cleanup.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+
+def main():
+    """Remove auto-generated python files"""
+    directory = Path.cwd().joinpath('prisma')
+    templates = Path.cwd().joinpath('prisma/generator/templates')
+    for template in templates.glob('[!_]*.py.jinja'):
+        path = directory.joinpath(template.name.rstrip('.jinja'))
+
+        if path.exists():
+            path.unlink()
+
+if __name__ == '__main__':
+    main()
