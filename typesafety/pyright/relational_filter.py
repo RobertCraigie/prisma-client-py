e93319479eb94107aea7a71f396288a3f9a57e9c
Robert Craigie <robertcraigie30@gmail.com>
2021-08-14 14:52:29 +0100

    fix(types): relational WhereInput types causes mypy SIGKILL
    this was "fixed" by broadening the relational filter sub-type to
    Dict[str, Any] when not targeting fully recursive types.
    
    Sacrificing the type safety of this is is something that I am okay with
    as full type safety can still be achieved by using pyright instead and
    the mypy limitation has been documented.
    
    this commit also does the following:
    
    - splits up the typesafety tests to different environments
    for different type checkers as it was frustrating how long the mypy
    typesafety tests would take
    
    - upgrades the mypy version we type check against to 0.910

---
+++ typesafety/pyright/relational_filter.py
@@ -0,0 +1,28 @@
+from prisma import Client
+
+
+async def one_to_one_relation(client: Client) -> None:
+    """Ensure relational filters are strongly typed with pyright"""
+    user = await client.user.find_first(
+        where={
+            'profile': {
+                'is': {
+                    'bio': {'contains': 'scotland'},
+                },
+            },
+        }
+    )
+    assert user is not None
+    reveal_type(user)  # T: User
+    reveal_type(user.id)  # T: str
+    reveal_type(user.profile)  # T: Profile | None
+
+    await client.user.find_first(
+        where={  # E: Argument of type "dict[str, dict[str, dict[str, str]]]" cannot be assigned to parameter "where" of type "UserWhereInput | None" in function "find_first"
+            'profile': {
+                'is': {
+                    'an_invalid_value': 'foo',
+                },
+            },
+        },
+    )
