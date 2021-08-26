4b7fb3b31f1d93b776a61c324477dc3485b12cd5
Robert Craigie <robertcraigie30@gmail.com>
2021-08-26 17:06:20 +0100

    fix(tests/integrations): correctly invalidate prisma package cache
    changes to the prisma package will now automatically be tested in
    integration tests without having to manually remove the cached venv

--- tests/integrations/recursive-types/test.sh
+++ tests/integrations/recursive-types/test.sh
@@ -9,7 +9,7 @@ source .venv/bin/activate
 set -x
 
 pip install -U 'pyright>=0.0.7'
-pip install -U --find-links=../../../.tests_cache/dist prisma-client[requests]
+pip install -U --force-reinstall --find-links=../../../.tests_cache/dist prisma-client[requests]
 
 TYPES=$(python -c 'import pathlib, prisma; print(pathlib.Path(prisma.__file__).parent / "types.py")')
 
