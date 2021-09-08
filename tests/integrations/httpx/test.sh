b6ad929246584d67493fc991f6cafae3b04c9bd2
Robert Craigie <robertcraigie30@gmail.com>
2021-09-08 09:17:11 +0100

    fix integration test

--- tests/integrations/httpx/test.sh
+++ tests/integrations/httpx/test.sh
@@ -8,8 +8,8 @@ set +x
 source .venv/bin/activate
 set -x
 
-pip install -U pytest pyright coverage==5.3.1
-pip install -U --force-reinstall --find-links=../../../.tests_cache/dist prisma-client[httpx]
+pip install -U pytest pyright coverage==5.3.1 -r ../../../requirements/httpx.txt
+pip install -U --force-reinstall ../../../.tests_cache/dist/*.whl
 
 # required due to https://github.com/RobertCraigie/prisma-client-py/issues/35
 HTTP=$(python -c 'import pathlib, prisma; print(pathlib.Path(prisma.__file__).parent / "http.py")')
