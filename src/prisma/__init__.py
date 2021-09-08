1f68e447058e831ea37c254844ae9cf7f52933b1
Robert Craigie <robertcraigie30@gmail.com>
2021-09-08 18:44:06 +0100

    feat(package): release v0.0.4

--- src/prisma/__init__.py
+++ src/prisma/__init__.py
@@ -5,7 +5,7 @@ __title__ = 'prisma'
 __author__ = 'RobertCraigie'
 __license__ = 'APACHE'
 __copyright__ = 'Copyright 2020-2021 RobertCraigie'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 from .utils import setup_logging
 from . import errors
