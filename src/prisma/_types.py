5430e00e882818b2cf3811e5b789a449082f6d83
Robert Craigie <robertcraigie30@gmail.com>
2021-11-13 11:24:44 +0000

    attempt to fix mypy by always using typing-extensions

--- src/prisma/_types.py
+++ src/prisma/_types.py
@@ -1,23 +1,12 @@
-import sys
 from typing import Callable, Coroutine, TypeVar, Any
+from typing_extensions import (
+    TypedDict as TypedDict,
+    Protocol as Protocol,
+    Literal as Literal,
+    runtime_checkable as runtime_checkable,
+)
 
 from pydantic import BaseModel
-
-
-if sys.version_info >= (3, 9, 2):
-    from typing import (  # pylint: disable=no-name-in-module, unused-import
-        TypedDict as TypedDict,
-        Protocol as Protocol,
-        Literal as Literal,
-        runtime_checkable as runtime_checkable,
-    )
-else:
-    from typing_extensions import (
-        TypedDict as TypedDict,
-        Protocol as Protocol,
-        Literal as Literal,
-        runtime_checkable as runtime_checkable,
-    )
 
 
 Method = Literal['GET', 'POST']
