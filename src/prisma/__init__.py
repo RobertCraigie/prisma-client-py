b22d3a0f465b4c7cda3a17bd210aa1fd1270ccc4
Robert Craigie <robertcraigie30@gmail.com>
2021-11-28 13:00:05 +0000

    chore(package): bump version

--- src/prisma/__init__.py
+++ src/prisma/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'prisma'
 __author__ = 'RobertCraigie'
 __license__ = 'APACHE'
 __copyright__ = 'Copyright 2020-2021 RobertCraigie'
-__version__ = '0.4.0'
+__version__ = '0.4.1a'
 
 from .utils import setup_logging
 from . import errors as errors
