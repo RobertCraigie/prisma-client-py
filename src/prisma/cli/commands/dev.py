769d12e8c8e563f7d9f6dc4466825886a9d1fc80
Robert Craigie <robertcraigie30@gmail.com>
2021-11-30 23:05:55 +0000

    refactor(engine): make the query engine interface abstract (#155)
    * refactor(engine): make the query engine interface abstract
    
    This will be useful for implementing support for the Prisma Data Proxy
    and for native Rust bindings
    
    * refactor engine shutdown interface
    
    * fix name error
    
    * chore: run prisma py dev tests under coverage

--- src/prisma/cli/commands/dev.py
+++ src/prisma/cli/commands/dev.py
@@ -1,9 +1,10 @@
-from typing import Optional
+from typing import Optional, cast
 
 import click
 
 from .. import options
 from ..utils import generate_client, error
+from ...engine import QueryEngine
 from ...utils import maybe_async_run, temp_env_update, module_exists
 
 
@@ -25,11 +26,14 @@ def playground(schema: Optional[str], skip_generate: bool) -> None:
     from prisma import Client
 
     client = Client()
+    engine_class = client._engine_class  # pylint: disable=protected-access
+    if engine_class.__name__ == 'QueryEngine':
+        with temp_env_update({'__PRISMA_PY_PLAYGROUND': '1'}):
+            maybe_async_run(client.connect)
 
-    with temp_env_update({'__PRISMA_PY_PLAYGROUND': '1'}):
-        maybe_async_run(client.connect)
-
-    # TODO: this is the result of a badly designed class
-    engine = client._engine  # pylint: disable=protected-access
-    assert engine.process is not None, 'Engine process unavailable for some reason'
-    engine.process.wait()
+        # TODO: this is the result of a badly designed class
+        engine = cast(QueryEngine, client._engine)  # pylint: disable=protected-access
+        assert engine.process is not None, 'Engine process unavailable for some reason'
+        engine.process.wait()
+    else:  # pragma: no cover
+        error(f'Unsupported engine type: "{engine_class}"')
