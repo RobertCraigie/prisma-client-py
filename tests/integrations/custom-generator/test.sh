4f2baef2f91f847f30ddfff283b519e6b18dc890
Robert Craigie <robertcraigie30@gmail.com>
2022-01-13 17:15:59 -0500

    chore: fix invalid config

--- tests/integrations/custom-generator/test.sh
+++ tests/integrations/custom-generator/test.sh
@@ -8,7 +8,8 @@ set +x
 source .venv/bin/activate
 set -x
 
-pip install -U 'pyright>=0.0.10' 'pytest'
+# TODO: pytest-asyncio should not be required
+pip install -U 'pyright>=0.0.10' 'pytest==6.2.4' pytest-asyncio==0.17.0
 pip install -U --force-reinstall ../../../.tests_cache/dist/*.whl
 
 rm -f generated.md 2.md
