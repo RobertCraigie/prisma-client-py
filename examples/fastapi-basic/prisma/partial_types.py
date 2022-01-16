dd0cdb9a40efde5878e222ef7cbc935f39cab2bc
Robert Craigie <robertcraigie30@gmail.com>
2022-01-16 13:13:51 -0500

    refactor(examples/fastapi): use model-based queries

--- examples/fastapi-basic/prisma/partial_types.py
+++ examples/fastapi-basic/prisma/partial_types.py
@@ -1,4 +1,4 @@
 from prisma.models import User, Post
 
-User.create_partial('UserWithoutRelations', exclude={'posts'})
-Post.create_partial('PostWithoutRelations', exclude={'author'})
+User.create_partial('UserWithoutRelations', exclude_relational_fields=True)
+Post.create_partial('PostWithoutRelations', exclude_relational_fields=True)
