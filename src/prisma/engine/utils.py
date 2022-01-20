ff838c0e4266d3c804204fa6915f3ef3e8566a88
HKGx <19597269+HKGx@users.noreply.github.com>
2022-01-20 22:47:05 +0100

    chore: comment out legacy ensure() function
    TODO: remove it?

--- src/prisma/engine/utils.py
+++ src/prisma/engine/utils.py
@@ -1,18 +1,12 @@
-import os
-import sys
-import time
 import socket
 import logging
-import subprocess
-from pathlib import Path
+
 from typing import NoReturn, Dict, Type, Any
 
 from . import errors
 from .. import errors as prisma_errors
 
 from ..http import Response
-from ..utils import time_since
-from ..binaries import GLOBAL_TEMP_DIR, ENGINE_VERSION, platform
 
 
 log: logging.Logger = logging.getLogger(__name__)
@@ -25,65 +19,67 @@ ERROR_MAPPING: Dict[str, Type[Exception]] = {
 }
 
 
-def ensure() -> Path:
-    start_time = time.monotonic()
-    file = None
-    force_version = True
-    binary_name = platform.check_for_extension(platform.binary_platform())
+# def ensure() -> Path:
+#     start_time = time.monotonic()
+#     file = None
+#     force_version = True
 
-    name = f'prisma-query-engine-{binary_name}'
-    local_path = Path.cwd().joinpath(name)
-    global_path = GLOBAL_TEMP_DIR.joinpath(name)
+#     binary_name = platform.check_for_extension(platform.binary_platform())
 
-    log.debug('Expecting local query engine %s', local_path)
-    log.debug('Expecting global query engine %s', global_path)
+#     name = f'prisma-query-engine-{binary_name}'
 
-    # TODO: this resolving should be moved to the binary class
-    binary = os.environ.get('PRISMA_QUERY_ENGINE_BINARY')
-    if binary:
-        log.debug('PRISMA_QUERY_ENGINE_BINARY is defined, using %s', binary)
+#     local_path = Path.cwd().joinpath(name)
+#     global_path = GLOBAL_TEMP_DIR.joinpath(name)
 
-        if not Path(binary).exists():
-            raise errors.BinaryNotFoundError(
-                'PRISMA_QUERY_ENGINE_BINARY was provided, '
-                f'but no query engine was found at {binary}'
-            )
+#     log.debug('Expecting local query engine %s', local_path)
+#     log.debug('Expecting global query engine %s', global_path)
 
-        file = Path(binary)
-        force_version = False
-    elif local_path.exists():
-        file = local_path
-        log.debug('Query engine found in the working directory')
-    elif global_path.exists():
-        file = global_path
-        log.debug('Query engine found in the global path')
+#     # TODO: this resolving should be moved to the binary class
+#     binary = os.environ.get('PRISMA_QUERY_ENGINE_BINARY')
+#     if binary:
+#         log.debug('PRISMA_QUERY_ENGINE_BINARY is defined, using %s', binary)
 
-    if not file:
-        raise errors.BinaryNotFoundError(
-            f'Expected {local_path} or {global_path} but neither were found.\n'
-            'Try running prisma py fetch'
-        )
+#         if not Path(binary).exists():
+#             raise errors.BinaryNotFoundError(
+#                 'PRISMA_QUERY_ENGINE_BINARY was provided, '
+#                 f'but no query engine was found at {binary}'
+#             )
 
-    start_version = time.monotonic()
-    process = subprocess.run(
-        [file.absolute(), '--version'], stdout=subprocess.PIPE, check=True
-    )
-    log.debug('Version check took %s', time_since(start_version))
+#         file = Path(binary)
+#         force_version = False
+#     elif local_path.exists():
+#         file = local_path
+#         log.debug('Query engine found in the working directory')
+#     elif global_path.exists():
+#         file = global_path
+#         log.debug('Query engine found in the global path')
 
-    version = (
-        str(process.stdout, sys.getdefaultencoding())
-        .replace('query-engine', '')
-        .strip()
-    )
-    log.debug('Using query engine version %s', version)
+#     if not file:
+#         raise errors.BinaryNotFoundError(
+#             f'Expected {local_path} or {global_path} but neither were found.\n'
+#             'Try running prisma py fetch'
+#         )
 
-    if force_version and version != ENGINE_VERSION:
-        raise errors.MismatchedVersionsError(expected=ENGINE_VERSION, got=version)
+#     start_version = time.monotonic()
+#     process = subprocess.run(
+#         [file.absolute(), '--version'], stdout=subprocess.PIPE, check=True
+#     )
+#     log.debug('Version check took %s', time_since(start_version))
 
-    log.debug('Using query engine at %s', file)
-    log.debug('Ensuring query engine took: %s', time_since(start_time))
+#     version = (
+#         str(process.stdout, sys.getdefaultencoding())
+#         .replace('query-engine', '')
+#         .strip()
+#     )
+#     log.debug('Using query engine version %s', version)
 
-    return file
+#     if force_version and version != ENGINE_VERSION:
+#         raise errors.MismatchedVersionsError(expected=ENGINE_VERSION, got=version)
+
+#     log.debug('Using query engine at %s', file)
+#     log.debug('Ensuring query engine took: %s', time_since(start_time))
+
+#     return file
 
 
 def get_open_port() -> int:
