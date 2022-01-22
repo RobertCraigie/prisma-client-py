d06b145587eabaea2de447715adaf6c1b84cc393
Robert Craigie <robertcraigie30@gmail.com>
2022-01-22 16:21:40 -0500

    chore: update docs and tests

--- tests/test_group_by.py
+++ tests/test_group_by.py
@@ -58,6 +58,7 @@ async def create_test_data(client: Client) -> None:
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_group_by(snapshot: SnapshotAssertion, client: Client) -> None:
+    """Basic test grouping by 1 field with no additional filters"""
     assert await client.user.group_by(['name']) == snapshot
     assert await client.profile.group_by(['country']) == snapshot
 
@@ -65,7 +66,9 @@ async def test_group_by(snapshot: SnapshotAssertion, client: Client) -> None:
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_docs_example(snapshot: SnapshotAssertion, client: Client) -> None:
-    # https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#groupby
+    """Test the example given in the Prisma documentation:
+    https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#groupby
+    """
     results = await client.profile.group_by(
         by=['country', 'city'],
         count={
@@ -95,8 +98,39 @@ async def test_docs_example(snapshot: SnapshotAssertion, client: Client) -> None
 async def test_order(
     snapshot: SnapshotAssertion, client: Client, order: SortOrder
 ) -> None:
+    """Test ordering results by a grouped field"""
     assert (
         await client.profile.group_by(['country'], order={'country': order}) == snapshot
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.persist_data
+async def test_order_multiple_fields(
+    snapshot: SnapshotAssertion, client: Client
+) -> None:
+    """Test ordering results by multiple fields is not support"""
+    with pytest.raises(prisma.errors.DataError):
+        await client.profile.group_by(
+            ['country', 'city'], order={'country': 'asc', 'city': 'desc'}
+        )
+
+
+@pytest.mark.asyncio
+@pytest.mark.persist_data
+async def test_order_mismatched_arguments(client: Client) -> None:
+    """The order argument only accepts fields that are being grouped"""
+    with pytest.raises(prisma.errors.InputError) as exc:
+        await client.profile.group_by(
+            ['city'],
+            order={  # pyright: reportGeneralTypeIssues=false
+                'country': 'asc',
+            },
+        )
+
+    assert exc.match(
+        r'Every field used for orderBy must be included in the by-arguments of the query\.'
+        + r'Missing fields: country'
     )
 
 
@@ -106,6 +140,7 @@ async def test_order(
 async def test_take(
     snapshot: SnapshotAssertion, client: Client, order: SortOrder
 ) -> None:
+    """Take argument limits number of records returned"""
     assert (
         await client.profile.group_by(['country'], take=1, order={'country': order})
         == snapshot
@@ -115,6 +150,7 @@ async def test_take(
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_take_missing_order_argument(client: Client) -> None:
+    """The order argument must be provided to use take"""
     with pytest.raises(TypeError) as exc:
         await client.profile.group_by(['country'], take=1)
 
@@ -129,6 +165,7 @@ async def test_take_missing_order_argument(client: Client) -> None:
 async def test_skip(
     snapshot: SnapshotAssertion, client: Client, order: SortOrder
 ) -> None:
+    """Skipping grouped records"""
     assert (
         await client.profile.group_by(['country'], skip=1, order={'country': order})
         == snapshot
@@ -138,6 +175,7 @@ async def test_skip(
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_skip_missing_order_argument(client: Client) -> None:
+    """The order argument must be provided to use skip"""
     with pytest.raises(TypeError) as exc:
         await client.profile.group_by(['country'], skip=1)
 
@@ -149,6 +187,7 @@ async def test_skip_missing_order_argument(client: Client) -> None:
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_where(client: Client) -> None:
+    """Where argument correctly filters records"""
     results = await client.profile.group_by(['country'], where={'country': 'Denmark'})
     assert len(results) == 1
     assert results[0].get('country') == 'Denmark'
@@ -163,9 +202,16 @@ async def test_where(client: Client) -> None:
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_having_missing_field_in_by(client: Client) -> None:
+    """Having filters must be an aggregation filter or be included in by"""
     with pytest.raises(prisma.errors.InputError) as exc:
         await client.profile.group_by(
-            by=['country'], count=True, having={'views': {'gt': 50}}
+            by=['country'],
+            count=True,
+            having={
+                'views': {
+                    'gt': 50,
+                },
+            },
         )
 
     assert exc.match(
@@ -177,6 +223,7 @@ async def test_having_missing_field_in_by(client: Client) -> None:
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_having_aggregation(snapshot: SnapshotAssertion, client: Client) -> None:
+    """Having aggregation filters records correctly"""
     assert (
         await client.profile.group_by(
             by=['country'],
@@ -212,6 +259,7 @@ async def test_having_aggregation(snapshot: SnapshotAssertion, client: Client) -
 async def test_having_aggregation_nested(
     snapshot: SnapshotAssertion, client: Client
 ) -> None:
+    """Having aggregation filters nested within statements correctly filters records"""
     results = await client.profile.group_by(
         by=['country'],
         count=True,
@@ -297,14 +345,22 @@ async def test_having_aggregation_nested(
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_count(snapshot: SnapshotAssertion, client: Client) -> None:
+    """Counting records"""
     assert await client.profile.group_by(['country'], count=True) == snapshot
     assert await client.profile.group_by(['country'], count={'_all': True}) == snapshot
     assert await client.profile.group_by(['country'], count={'city': True}) == snapshot
+    assert (
+        await client.profile.group_by(
+            ['country'], count={'city': True, 'country': True}
+        )
+        == snapshot
+    )
 
 
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_avg(snapshot: SnapshotAssertion, client: Client) -> None:
+    """Getting the average of records"""
     assert await client.profile.group_by(['country'], avg={'views': True}) == snapshot
     assert (
         await client.types.group_by(['string'], avg={'integer': True, 'bigint': True})
@@ -315,16 +371,19 @@ async def test_avg(snapshot: SnapshotAssertion, client: Client) -> None:
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_sum(snapshot: SnapshotAssertion, client: Client) -> None:
+    """Getting the sum of records"""
     assert await client.profile.group_by(['country'], sum={'views': True}) == snapshot
 
 
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_min(snapshot: SnapshotAssertion, client: Client) -> None:
+    """Getting the minimum value of records"""
     assert await client.profile.group_by(['country'], min={'views': True}) == snapshot
 
 
 @pytest.mark.asyncio
 @pytest.mark.persist_data
 async def test_max(snapshot: SnapshotAssertion, client: Client) -> None:
+    """Getting the maximum value of records"""
     assert await client.profile.group_by(['country'], max={'views': True}) == snapshot
