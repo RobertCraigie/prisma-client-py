234bcf53102dc9d5da4993b0cb2e95f75bf6f2b4
Robert Craigie <robertcraigie30@gmail.com>
2022-01-25 19:18:16 -0500

    feat: support combined unique constraints

--- tests/test_querying/test_records.py
+++ tests/test_querying/test_records.py
@@ -1,5 +1,5 @@
 import pytest
-from prisma.models import User
+from prisma.models import Record, User
 
 
 # TODO: Base64 and Bytes fields
@@ -40,6 +40,50 @@ async def test_update_1_to_many() -> None:
     updated = await user.record().update()
     print(updated.json(indent=2))
     assert False
+
+
+class MyUser(User, warn_subclass=False):
+    foo: str = 'bar'
+
+
+@pytest.mark.asyncio
+@pytest.mark.prisma
+async def test_update_subclass() -> None:
+    """Updating a model that has been subclassed"""
+    user = await MyUser.prisma().create({'name': 'Robert'})
+    assert user.name == 'Robert'
+
+    user.foo = 'Wow'
+    user.name = 'Tegan'
+    assert user.name == 'Tegan'
+
+    updated = await user.record().update()
+    assert updated.id == user.id
+
+
+@pytest.mark.asyncio
+@pytest.mark.prisma
+async def test_update_combined_unique_constraint() -> None:
+    """Updating a model that uses a combined unique constraint"""
+    record = await Record.prisma().create(
+        data={
+            'first': 'Foo',
+            'last': 'Bar',
+        },
+    )
+    record.first = 'Baz'
+    updated = await record.record().update()
+    assert updated.first == 'Baz'
+
+    found = await Record.prisma().find_unique(
+        where={
+            'first_last': {
+                'first': 'Baz',
+                'last': 'Bar',
+            },
+        },
+    )
+    assert found is not None
 
 
 # TODO: test relational fields
