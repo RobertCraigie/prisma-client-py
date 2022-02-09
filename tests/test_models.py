e0b57eacfb39c6b0cc7735fb97b5a21e3a4e77af
Robert Craigie <robert@craigie.dev>
2022-02-08 19:31:11 -0500

    feat(models): do not raise unsupported subclass warning with FastAPI

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,6 @@
 import pytest
+from fastapi import FastAPI
+
 from prisma.models import User
 from prisma.errors import UnsupportedSubclassWarning
 
@@ -280,3 +282,21 @@ def test_subclassing_warns() -> None:
     # ensure other arguments can be passed to support multiple inheritance
     class MyUser3(User, warn_subclass=False, foo=1):  # pyright: reportUnusedClass=false
         pass
+
+
+# NOTE: we only include a FastAPI test here as it is a small dependency
+# if we need to ignore warnings for other packages, we do not need unit tests
+
+
+def test_subclass_ignores_fastapi_response_model() -> None:
+    """FastAPI implicitly creates new types from the model passed to response_model.
+    This calls __init_subclass__ which will raise warnings that users cannot do anything about.
+    """
+    # this test may look like it does not test anything but as we turn
+    # warnings into errors when running pytest, this will catch any
+    # warnings we raise
+    app = FastAPI()
+
+    @app.get('/', response_model=User)
+    async def _() -> None:  # pragma: no cover
+        ...
