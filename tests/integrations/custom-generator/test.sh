b2b19a86c00a4582e9c094899281575b118cc530
Robert Craigie <robert@craigie.dev>
2022-02-26 18:08:06 -0500

    chore(deps/pyright): bump version

--- tests/integrations/custom-generator/test.sh
+++ tests/integrations/custom-generator/test.sh
@@ -8,8 +8,7 @@ set +x
 source .venv/bin/activate
 set -x
 
-# TODO: pytest-asyncio should not be required
-pip install -U 'pyright>=0.0.10' 'pytest==6.2.4' pytest-asyncio==0.17.0
+pip install -U -r requirements.txt
 pip install -U --force-reinstall ../../../.tests_cache/dist/*.whl
 
 rm -f generated.md 2.md
