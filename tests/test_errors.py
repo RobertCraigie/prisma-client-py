c7cc93ab51c1e701be9085c71bf34d0328fd3fb6
Robert Craigie <robert@craigie.dev>
2022-04-21 17:42:14 -0400

    feat(client): add FieldNotFoundError

---
+++ tests/test_errors.py
@@ -0,0 +1,30 @@
+import pytest
+
+from prisma import Prisma
+from prisma.errors import FieldNotFoundError
+
+
+@pytest.mark.asyncio
+async def test_field_not_found_error(client: Prisma) -> None:
+    with pytest.raises(FieldNotFoundError, match='bad_field'):
+        await client.post.find_first(where={'bad_field': 'foo'})  # type: ignore
+
+    with pytest.raises(FieldNotFoundError, match='foo'):
+        await client.post.create(
+            data={  # type: ignore
+                'title': 'foo',
+                'published': True,
+                'foo': 'bar',
+            }
+        )
+
+    with pytest.raises(FieldNotFoundError, match='foo'):
+        await client.post.find_first(
+            where={
+                'author': {
+                    'is': {
+                        'foo': 'bar',
+                    },
+                },
+            },
+        )
