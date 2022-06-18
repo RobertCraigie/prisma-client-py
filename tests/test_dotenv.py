62e1bd63814615975d39f1b22cb17bb45df37978
Robert Craigie <robert@craigie.dev>
2022-06-18 14:18:48 +0100

    fix(client): do not override present system env variables

--- tests/test_dotenv.py
+++ tests/test_dotenv.py
@@ -7,20 +7,23 @@ from .utils import Testdir
 
 
 ENV_KEY = '_PRISMA_PY_TESTING_DOTENV_DATABSE_URL'
+DEFAULT_VALUE = 'file:dev.db'
 
 
-def make_env_file(testdir: Testdir, name: str) -> None:
+def make_env_file(testdir: Testdir, name: str = '.env') -> None:
     path = testdir.path.joinpath(name)
     if not path.parent.exists():
         path.parent.mkdir(parents=True)
 
     with path.open('w') as file:
-        file.write(f'export {ENV_KEY}="file:dev.db"')
+        file.write(f'export {ENV_KEY}="{DEFAULT_VALUE}"')
 
 
 @pytest.fixture(autouse=True)
-def clear_env() -> None:
+def clear_env(testdir: Testdir) -> None:
     os.environ.pop(ENV_KEY, None)
+    testdir.path.joinpath('.env').unlink(missing_ok=True)
+    testdir.path.joinpath('prisma/.env').unlink(missing_ok=True)
 
 
 @pytest.mark.parametrize('name', ['.env', 'prisma/.env'])
@@ -40,3 +43,18 @@ def test_load_env_no_files(testdir: Testdir) -> None:
     assert len(list(testdir.path.iterdir())) == 0
     load_env()
     assert ENV_KEY not in os.environ
+
+
+def test_system_env_takes_priority(testdir: Testdir) -> None:
+    """When an environment variable is present in both the system env and the .env file,
+    the system env should take priority.
+    """
+    make_env_file(testdir)
+    os.environ[ENV_KEY] = 'foo'
+
+    Prisma()
+    assert os.environ[ENV_KEY] == 'foo'
+
+    os.environ.pop(ENV_KEY)
+    Prisma()
+    assert os.environ[ENV_KEY] == DEFAULT_VALUE
