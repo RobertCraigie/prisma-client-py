5c9c9369590f25034e4f3798cc03a33269eaac01
kfields <kurtisfields@gmail.com>
2022-07-01 08:24:27 -0600

    fix raw

--- src/prisma/raw.py
+++ src/prisma/raw.py
@@ -1,8 +1,44 @@
-# from pydantic import BaseModel
+from array import array
+import binascii
 from datetime import datetime
 from ._types import BaseModelT
 
 from typing import Any, Dict, Optional, Type, Union, List
+
+# From: https://github.com/prisma/prisma/blob/main/packages/client/src/runtime/utils/deserializeRawResults.ts
+"""
+type PrismaType =
+  | 'int'
+  | 'bigint'
+  | 'float'
+  | 'double'
+  | 'string'
+  | 'enum'
+  | 'bytes'
+  | 'bool'
+  | 'char'
+  | 'decimal'
+  | 'json'
+  | 'xml'
+  | 'uuid'
+  | 'datetime'
+  | 'date'
+  | 'time'
+  | 'array'
+  | 'null'
+"""
+
+"""
+export function deserializeRawResults(rows: Array<Record<string, TypedValue>>): unknown[] {
+  return rows.map((row) => {
+    const mappedRow = {} as Record<string, unknown>
+    for (const key of Object.keys(row)) {
+      mappedRow[key] = deserializeValue(row[key])
+    }
+    return mappedRow
+  })
+}
+"""
 
 
 def parse_raw_list(
@@ -27,32 +63,44 @@ def parse_raw_obj(
 def parse_raw_value(raw_value: Dict[Any, Any]):
     _type = raw_value['prisma__type']
     value = raw_value['prisma__value']
-    # return value
+
     if _type in _casts:
         return _casts[_type](value)
+    else:
+        return value
 
 
-def _cast_any(value: Any):
-    return value
+def _cast_datetime(value: str):
+    return datetime.fromisoformat(value)
 
 
-def _cast_datetime(value: Any):
-    if isinstance(value, str):
-        return datetime.fromisoformat(str(value))
-    return value
+def _cast_bigint(value: str):
+    return int(value)
 
 
-def _cast_bigint(value: Any):
-    if isinstance(value, str):
-        return int(value)
-    return value
+def _cast_bytes(value: str):
+    return binascii.a2b_base64(value)
+
+
+def _cast_decimal(value: str):
+    return float(value)
+
+
+def _cast_time(value: str):
+    # return new Date(`1970-01-01T${value}Z`)
+    return datetime.fromisoformat(f'1970-01-01T${value}Z')
+
+
+def _cast_array(value: list[Any]) -> Union[list[Any], Any]:
+    return map(parse_raw_value, value)
 
 
 _casts = {
-    'null': _cast_any,
-    'bool': _cast_any,
-    'string': _cast_any,
-    'int': _cast_any,
-    'datetime': _cast_datetime,
     'bigint': _cast_bigint,
+    'bytes': _cast_bytes,
+    'decimal': _cast_decimal,
+    'datetime': _cast_datetime,
+    'date': _cast_datetime,
+    'time': _cast_time,
+    'array': _cast_array,
 }
