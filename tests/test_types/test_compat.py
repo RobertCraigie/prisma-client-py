e3d625a2f2a1c5d5536543b4111dcedf4a3902b4
Robert Craigie <robert@craigie.dev>
2022-09-03 09:15:15 +0100

    Fix linters

--- tests/test_types/test_compat.py
+++ tests/test_types/test_compat.py
@@ -4,5 +4,6 @@ from prisma.models import Types
 
 
 def test_json_schema_compatible(snapshot: SnapshotAssertion) -> None:
+    """Ensure a JSON Schema can be created for all types"""
     schema = Types.schema()
     assert schema == snapshot
