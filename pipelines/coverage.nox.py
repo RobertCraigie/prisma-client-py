ddbc5b0172b18fd3a92d7ca59cd1e9f952b2b287
Robert Craigie <robert@craigie.dev>
2022-09-04 14:19:53 +0100

    Try fix coverage

--- pipelines/coverage.nox.py
+++ pipelines/coverage.nox.py
@@ -1,14 +1,18 @@
 import nox
 
+from pipelines.utils import setup_env
+
 
 @nox.session
 def setup(session: nox.Session) -> None:
+    setup_env(session)
     session.install('coverage==5.3.1')
     session.run('coverage', 'erase')
 
 
 @nox.session
 def report(session: nox.Session) -> None:
+    setup_env(session)
     session.install('coverage==5.3.1')
     session.run('coverage', 'combine')
     session.run('coverage', 'html', '-i')
