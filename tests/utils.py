c80e2be3eb8d49583abf86ae8226dc564395d7b2
Robert Craigie <robert@craigie.dev>
2022-10-02 01:22:51 +0100

    forgot to commit changes

--- tests/utils.py
+++ tests/utils.py
@@ -319,9 +319,7 @@ def assert_time_like_now(dt: datetime, threshold: int = 10) -> None:
     # have to remove the timezone details as utcnow() is not timezone aware
     # and we cannot subtract a timezone aware datetime from a non timezone aware datetime
     dt = dt.replace(tzinfo=None)
-    delta = datetime.utcnow() - dt
-    assert delta.days == 0
-    assert delta.total_seconds() < threshold
+    assert_similar_time(dt, datetime.utcnow(), threshold=threshold)
 
 
 def escape_path(path: Union[str, Path]) -> str:
