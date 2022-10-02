8cfd673fcefa7f5fc5baea60bdc848998c10f513
Robert Craigie <robert@craigie.dev>
2022-10-02 11:56:09 +0100

    Fix tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -80,7 +80,7 @@ def test_engine_binary_does_not_exist(monkeypatch: MonkeyPatch) -> None:
 def test_mismatched_version_error(fake_process: FakeProcess) -> None:
     """Mismatched query engine versions raises an error"""
     fake_process.register_subprocess(
-        [QUERY_ENGINE.path, '--version'],  # type: ignore[list-item]
+        [str(QUERY_ENGINE.path), '--version'],
         stdout='query-engine unexpected-hash',
     )
 
@@ -102,14 +102,14 @@ def test_ensure_local_path(
     fake_engine.touch()
 
     fake_process.register_subprocess(
-        [fake_engine, '--version'],  # type: ignore[list-item]
+        [str(fake_engine), '--version'],
         stdout='query-engine a-different-hash',
     )
     with pytest.raises(errors.MismatchedVersionsError):
         path = utils.ensure()
 
     fake_process.register_subprocess(
-        [fake_engine, '--version'],  # type: ignore[list-item]
+        [str(fake_engine), '--version'],
         stdout=f'query-engine {config.engine_version}',
     )
     path = utils.ensure()
@@ -124,7 +124,7 @@ def test_ensure_env_override(
     fake_engine.touch()
 
     fake_process.register_subprocess(
-        [fake_engine, '--version'],  # type: ignore[list-item]
+        [str(fake_engine), '--version'],
         stdout='query-engine a-different-hash',
     )
 
