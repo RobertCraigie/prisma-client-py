c0364b808f00a7e2b7707e300d2a600fa90812fd
Robert Craigie <robert@craigie.dev>
2022-10-02 11:24:12 +0100

    Update script

--- scripts/ci.py
+++ scripts/ci.py
@@ -1,5 +1,21 @@
-import json
 import os
+import sys
+import subprocess
 
-print(json.dumps(dict(os.environ), indent=2))
-raise RuntimeError()
+
+PYTHON_MAPPING = {
+    '3.7': '3.7',
+    '3.8': '3.8',
+    '3.9': '3.9',
+    '3.10': '3.10',
+    '3.11.0-rc.1': '3.11',
+}
+
+
+def main() -> None:
+    python_version = PYTHON_MAPPING[os.environ['TARGET_PYTHON']]
+    subprocess.check_call(['nox', '-p', python_version, *sys.argv[1:]])
+
+
+if __name__ == '__main__':
+    main()
