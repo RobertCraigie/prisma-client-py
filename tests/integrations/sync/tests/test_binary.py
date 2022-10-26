de6c4789f3a0b07de24508c80eebfe77edfd2631
Jacob Roberts <jacobdr@gmail.com>
2022-10-26 10:21:48 -0700

    Use mocking for the unit tests to download binaries. Pass through coverage context in CI. Add test timeout. Capture test durations for longest running tests

--- tests/integrations/sync/tests/test_binary.py
+++ tests/integrations/sync/tests/test_binary.py
@@ -1,14 +1,23 @@
-import random
-from prisma.binaries import BINARIES
+import pathlib
+from pytest_mock import MockerFixture
+
+from prisma.binaries.binaries import PRISMA_FORMAT_BINARY
 
 
-def test_download() -> None:
+def test_download(
+    mocker: MockerFixture,
+    tmp_path: pathlib.Path,
+) -> None:
     """Binary can be downloaded"""
-    binary = random.choice(BINARIES)
-    assert binary.path.exists()
-    binary.path.unlink()
-    assert not binary.path.exists()
+    # Always use the format engine binary since its quite small relative
+    # to other binaries.
+    # NOTE: unlike the test_fetch tetsts, this is not mocked and will
+    # make the network transfer. We do it in a separate directory so
+    # as to not create issues for other tests if we start parallelizing
+    mocker.patch('prisma.config.binary_cache_dir', tmp_path)
 
+    binary = PRISMA_FORMAT_BINARY
+    assert not binary.path.exists()
     binary.download()
 
     assert binary.path.exists()
