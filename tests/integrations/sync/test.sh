12cf22d063f46502d5ac7e1d1f498a83c36f56a0
Jacob Roberts <jacobdr@gmail.com>
2022-10-26 10:37:12 -0700

    Update the coverage dependency

--- tests/integrations/sync/test.sh
+++ tests/integrations/sync/test.sh
@@ -9,7 +9,7 @@ source .venv/bin/activate
 set -x
 
 # TODO: pytest-asyncio should not be required
-pip install -U pytest==6.2.4 pytest-asyncio==0.17.0 pyright coverage==6.1.1
+pip install -U pytest==6.2.4 pytest-asyncio==0.17.0 pyright coverage==6.5.0
 pip install -U --force-reinstall ../../../.tests_cache/dist/*.whl
 
 # required due to https://github.com/RobertCraigie/prisma-client-py/issues/35
