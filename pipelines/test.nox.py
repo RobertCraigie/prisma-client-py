97a900bd2664e814cbc7a344a45125805f3ec348
Robert Craigie <robert@craigie.dev>
2022-11-13 14:32:17 +0000

    Debug alpine

--- pipelines/test.nox.py
+++ pipelines/test.nox.py
@@ -1,4 +1,5 @@
 import nox
+import distro
 
 from pipelines.utils import setup_env
 from pipelines.utils.prisma import generate
@@ -7,10 +8,13 @@ from pipelines.utils.prisma import generate
 @nox.session(python=['3.7', '3.8', '3.9', '3.10', '3.11'])
 def test(session: nox.Session) -> None:
     setup_env(session)
+
+    print(distro.info())
+
     session.install('-r', 'pipelines/requirements/test.txt')
-    session.install('-r', 'pipelines/requirements/node.txt')
     session.install('.')
 
+    session.install('-r', 'pipelines/requirements/node.txt')
     generate(session)
 
     session.run(
