bac298474484b277bfc67c11d31a45f5123a91ba
Robert Craigie <robert@craigie.dev>
2022-11-26 12:18:52 +0000

    Use coverage.py's reporting instead

--- pipelines/coverage.nox.py
+++ pipelines/coverage.nox.py
@@ -1,3 +1,6 @@
+import webbrowser
+from pathlib import Path
+
 import nox
 
 from pipelines.utils import setup_env, CACHE_DIR
@@ -10,11 +13,40 @@ def setup(session: nox.Session) -> None:
     session.run('coverage', 'erase')
 
 
-@nox.session
-def report(session: nox.Session) -> None:
+def _setup_report(session: nox.Session) -> None:
+    # TODO: generate in place for coverage parsing?
     session.env['COVERAGE_FILE'] = str(CACHE_DIR / '.coverage')
     session.install('-r', 'pipelines/requirements/coverage.txt')
-    session.run('coverage', 'combine')
+
+    if '--no-combine' not in session.posargs:
+        session.run('coverage', 'combine')
+
     session.run('coverage', 'html', '-i')
     session.run('coverage', 'xml', '-i')
+
+    if '--open' in session.posargs:
+        url = f'file://{Path.cwd() / "htmlcov" / "index.html"}'
+        print(f'Opening browser at {url}')
+        webbrowser.open(url)
+
+
+@nox.session
+def report(session: nox.Session) -> None:
+    _setup_report(session)
+
     session.run('coverage', 'report', '-i', '--skip-covered')
+
+
+@nox.session(name='report-strict')
+def report_strict(session: nox.Session) -> None:
+    """Like `report` but will error if coverage doesn't meet certain requirements"""
+    _setup_report(session)
+
+    session.run(
+        'coverage',
+        'report',
+        '-i',
+        '--skip-covered',
+        '--include=tests/**',
+        '--fail-under=100',
+    )
