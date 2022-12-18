de3c343bfb268b73206d446de8a77cce6e211592
Robert Craigie <robert@craigie.dev>
2022-12-18 16:01:34 -0500

    Fix

---
+++ tests/test_generation/exhaustive/partials.py
@@ -0,0 +1,22 @@
+from prisma.models import Post, User
+
+
+Post.create_partial('PostWithAuthor', include={'id', 'title', 'author'})
+
+User.create_partial('UserOnlyName', include={'name'})
+Post.create_partial(
+    'PostWithCustomAuthor',
+    include={'id', 'title', 'author'},
+    relations={
+        'author': 'UserOnlyName',
+    },
+)
+
+User.create_partial('UserWithPosts', include={'name', 'posts'})
+Post.create_partial(
+    'PostWithNestedRelations',
+    include={'id', 'title', 'author'},
+    relations={
+        'author': 'UserWithPosts',
+    },
+)
