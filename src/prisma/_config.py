81a73193989ff1927f3fa4a0858fb160ff96585a
Robert Craigie <robert@craigie.dev>
2022-12-24 22:54:39 +0000

    Bump to `v4.8.0`

--- src/prisma/_config.py
+++ src/prisma/_config.py
@@ -18,13 +18,13 @@ class DefaultConfig(BaseSettings):
     #       doesn't change then the CLI is incorrectly cached
     prisma_version: str = Field(
         env='PRISMA_VERSION',
-        default='4.7.1',
+        default='4.8.0',
     )
 
     # Engine binary versions can be found under https://github.com/prisma/prisma-engine/commits/main
     expected_engine_version: str = Field(
         env='PRISMA_EXPECTED_ENGINE_VERSION',
-        default='272861e07ab64f234d3ffc4094e32bd61775599c',
+        default='d6e67a83f971b175a593ccc12e15c4a757f93ffe',
     )
 
     # Home directory, used to build the `binary_cache_dir` option by default, useful in multi-user
