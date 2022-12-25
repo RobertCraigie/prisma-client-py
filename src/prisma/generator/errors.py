70cfe3f93fbb2cd009a4c98c46964d4dde33f87c
Robert Craigie <robert@craigie.dev>
2022-12-25 13:16:36 +0000

    Remove now dead code

--- src/prisma/generator/errors.py
+++ src/prisma/generator/errors.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 from ..errors import PrismaError
 
 
@@ -7,22 +5,5 @@ class TemplateError(PrismaError):
     pass
 
 
-class CompoundConstraintError(ValueError):
-    def __init__(self, constraint: Union['PrimaryKey', 'UniqueIndex']) -> None:
-        if isinstance(constraint, PrimaryKey):
-            annotation = '@@id'
-        else:
-            annotation = '@@unique'
-
-        super().__init__(
-            f'Compound constraint with name: {constraint.name} is already used as a name for a field; '
-            'Please choose a different name. For example: \n'
-            f'  {annotation}([{", ".join(constraint.fields)}], name: "my_custom_primary_key")'
-        )
-
-
 class PartialTypeGeneratorError(PrismaError):
     pass
-
-
-from .models import PrimaryKey, UniqueIndex
