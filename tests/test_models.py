ddedcb99366511e2218af688c59720f6cb0e80ed
Robert Craigie <robert@craigie.dev>
2023-02-02 22:30:00 +0000

    Workaround mypy

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,16 +10,24 @@ from prisma.models import User
 
 def test_subclass_warn_subclass_argument_deprecated() -> None:
     """The class argument `wan_subclass` is deprecated"""
-    with pytest.warns(
-        DeprecationWarning, match='The `warn_subclass` argument is deprecated'
-    ):
+    # For some indiscernible reason, this code causes mypy to crash...
+    #
+    # The `exclude` option doesn't help us because of the way mypy works it still causes it to crash.
+    #
+    # I do not have the time nor the willpower to investigate why this happens so we resort to this solution.
+    if not TYPE_CHECKING:
+        with pytest.warns(
+            DeprecationWarning,
+            match='The `warn_subclass` argument is deprecated',
+        ):
 
-        class MyUser(User, warn_subclass=False):
-            pass
+            class MyUser(User, warn_subclass=False):
+                pass
 
-    with pytest.warns(
-        DeprecationWarning, match='The `warn_subclass` argument is deprecated'
-    ):
+        with pytest.warns(
+            DeprecationWarning,
+            match='The `warn_subclass` argument is deprecated',
+        ):
 
-        class MyUser2(User, warn_subclass=True):
-            pass
+            class MyUser2(User, warn_subclass=True):
+                pass
