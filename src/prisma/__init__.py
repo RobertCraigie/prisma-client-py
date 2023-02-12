13f5c47812bb333ea57426145d4bb7bf997347ba
Robert Craigie <robert@craigie.dev>
2023-02-12 16:01:29 +0000

    chore(package): bump version

--- src/prisma/__init__.py
+++ src/prisma/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'prisma'
 __author__ = 'RobertCraigie'
 __license__ = 'APACHE'
 __copyright__ = 'Copyright 2020-2021 RobertCraigie'
-__version__ = '0.8.1'
+__version__ = '0.8.2a'
 
 from typing import TYPE_CHECKING
 
