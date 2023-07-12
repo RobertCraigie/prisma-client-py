548dba3fb2c9ee8a794830f7528af5d0e6393535
Robert Craigie <robert@craigie.dev>
2023-07-12 12:19:05 +0100

    feat(prisma): upgrade to v5.0.0

--- src/prisma/_config.py
+++ src/prisma/_config.py
@@ -18,13 +18,13 @@ class DefaultConfig(BaseSettings):
     #       doesn't change then the CLI is incorrectly cached
     prisma_version: str = Field(
         env='PRISMA_VERSION',
-        default='4.15.0',
+        default='5.0.0',
     )
 
     # Engine binary versions can be found under https://github.com/prisma/prisma-engine/commits/main
     expected_engine_version: str = Field(
         env='PRISMA_EXPECTED_ENGINE_VERSION',
-        default='8fbc245156db7124f997f4cecdd8d1219e360944',
+        default='6b0aef69b7cdfc787f822ecd7cdc76d5f1991584',
     )
 
     # Home directory, used to build the `binary_cache_dir` option by default, useful in multi-user
