144a407cfb4b9a3812e86ad4dffd09f58cdae6b6
Robert Craigie <robert@craigie.dev>
2024-01-14 18:17:25 +0000

    feat(package): release v0.12.0

--- src/prisma/__init__.py
+++ src/prisma/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'prisma'
 __author__ = 'RobertCraigie'
 __license__ = 'APACHE'
 __copyright__ = 'Copyright 2020-2023 RobertCraigie'
-__version__ = '0.11.0'
+__version__ = '0.12.0'
 
 from typing import TYPE_CHECKING
 
