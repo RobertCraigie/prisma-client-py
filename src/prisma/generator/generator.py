e0faebcf7656a692bf63123122afc4bcee4baa33
Owen <33508519+lxxonx@users.noreply.github.com>
2024-01-23 15:57:02 +0900

    test: snapshot update

--- src/prisma/generator/generator.py
+++ src/prisma/generator/generator.py
@@ -51,7 +51,7 @@ DEFAULT_ENV = Environment(
     lstrip_blocks=True,
     loader=FileSystemLoader(Path(__file__).parent / 'templates'),
     undefined=StrictUndefined,
-    extensions=['jinja2_strcase.StrcaseExtension']
+    extensions=['jinja2_strcase.StrcaseExtension'],
 )
 
 # the type: ignore is required because Jinja2 filters are not typed
