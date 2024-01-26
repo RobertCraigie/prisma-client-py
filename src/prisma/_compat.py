99d80ede873525ce2862d2bc093962e687af623c
Fisher60 <wakefollower@gmail.com>
2024-01-25 19:24:14 -0600

    add 'as StrEnum' import back to fix lint errors.
    This was erroneously removed and should now be correct again.

--- src/prisma/_compat.py
+++ src/prisma/_compat.py
@@ -340,9 +340,9 @@ else:
 
 
 if sys.version_info < (3, 11):
-    from strenum import StrEnum
+    from strenum import StrEnum as StrEnum
 else:
-    from enum import StrEnum
+    from enum import StrEnum as StrEnum
 
 
 def removeprefix(string: str, prefix: str) -> str:
