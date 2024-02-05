008deed4cc5b1307b0d8c4a28f480c76e08df41d
lxxonx <leeonechang92@gmail.com>
2024-02-06 00:59:54 +0900

    test(client casing): add casing util tests

--- tests/test_generation/test_utils.py
+++ tests/test_generation/test_utils.py
@@ -1,4 +1,4 @@
-from prisma.generator.utils import copy_tree
+from prisma.generator.utils import copy_tree, to_camel_case, to_pascal_case, to_snake_case
 
 from ..utils import Testdir
 
@@ -26,3 +26,40 @@ def test_copy_tree_ignores_files(testdir: Testdir) -> None:
     assert files[0].name == 'bar.py'
     assert files[1].name == 'foo.py'
     assert files[2].name == 'hello.py'
+
+
+def test_to_snake_case() -> None:
+    snake_case = "snake_case_test"
+    pascal_case = "PascalCaseTest"
+    camel_case = "camelCaseTest"
+    # TODO: implement mixed case
+    # mixed_case = "Mixed_Case_Test" # mixed case is not supported
+
+    assert to_snake_case(snake_case) == "snake_case_test"
+    assert to_snake_case(pascal_case) == "pascal_case_test"
+    assert to_snake_case(camel_case) == "camel_case_test"
+    # assert to_snake_case(mixed_case) == "mixed_case_test" # output: mixed__case__test
+
+
+def test_to_pascal_case() -> None:
+    snake_case = "snake_case_test"
+    pascal_case = "PascalCaseTest"
+    camel_case = "camelCaseTest"
+    mixed_case = "Mixed_Case_Test"
+
+    assert to_pascal_case(snake_case) == "SnakeCaseTest"
+    assert to_pascal_case(pascal_case) == "PascalCaseTest"
+    assert to_pascal_case(camel_case) == "CamelCaseTest"
+    assert to_pascal_case(mixed_case) == "MixedCaseTest"
+
+
+def test_to_camel_case() -> None:
+    snake_case = "snake_case_test"
+    pascal_case = "PascalCaseTest"
+    camel_case = "camelCaseTest"
+    mixed_case = "Mixed_Case_Test"
+
+    assert to_camel_case(snake_case) == "snakeCaseTest"
+    assert to_camel_case(pascal_case) == "pascalCaseTest"
+    assert to_camel_case(camel_case) == "camelCaseTest"
+    assert to_camel_case(mixed_case) == "mixedCaseTest"
