1fa93317a6047c8b7ab8dc26418074eba28228d9
Robert Craigie <robert@craigie.dev>
2024-02-24 20:46:08 +0000

    chore(internal): add change case util functions
    Co-Authored-By: lxxonx <leeonechang92@gmail.com>

--- src/prisma/generator/utils.py
+++ src/prisma/generator/utils.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import os
+import re
 import shutil
 from typing import TYPE_CHECKING, Any, Dict, List, Union, TypeVar, Iterator
 from pathlib import Path
@@ -122,3 +125,46 @@ def clean_multiline(string: str) -> str:
     assert string, 'Expected non-empty string'
     lines = string.splitlines()
     return '\n'.join([dedent(lines[0]), *lines[1:]])
+
+
+# https://github.com/nficano/humps/blob/master/humps/main.py
+
+ACRONYM_RE = re.compile(r'([A-Z\d]+)(?=[A-Z\d]|$)')
+PASCAL_RE = re.compile(r'([^\-_]+)')
+SPLIT_RE = re.compile(r'([\-_]*[A-Z][^A-Z]*[\-_]*)')
+UNDERSCORE_RE = re.compile(r'(?<=[^\-_])[\-_]+[^\-_]')
+
+
+def to_snake_case(input_str: str) -> str:
+    if to_camel_case(input_str) == input_str or to_pascal_case(input_str) == input_str:  # if camel case or pascal case
+        input_str = ACRONYM_RE.sub(lambda m: m.group(0).title(), input_str)
+        input_str = '_'.join(s for s in SPLIT_RE.split(input_str) if s)
+        return input_str.lower()
+    else:
+        input_str = re.sub(r'[^a-zA-Z0-9]', '_', input_str)
+        input_str = input_str.lower().strip('_')
+
+        return input_str
+
+
+def to_camel_case(input_str: str) -> str:
+    if len(input_str) != 0 and not input_str[:2].isupper():
+        input_str = input_str[0].lower() + input_str[1:]
+    return UNDERSCORE_RE.sub(lambda m: m.group(0)[-1].upper(), input_str)
+
+
+def to_pascal_case(input_str: str) -> str:
+    def _replace_fn(match: re.Match[str]) -> str:
+        return match.group(1)[0].upper() + match.group(1)[1:]
+
+    input_str = to_camel_case(PASCAL_RE.sub(_replace_fn, input_str))
+    return input_str[0].upper() + input_str[1:] if len(input_str) != 0 else input_str
+
+
+def to_constant_case(input_str: str) -> str:
+    """Converts to snake case + uppercase, examples:
+
+    foo_bar -> FOO_BAR
+    fooBar -> FOO_BAR
+    """
+    return to_snake_case(input_str).upper()
