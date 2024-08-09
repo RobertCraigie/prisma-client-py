8f56fc595cffe910489bdf7a0cefdbd6069fe8b2
Adeel Khan <adeel@dv01.co>
2024-08-09 18:13:35 -0400

    remove search_not

--- databases/tests/test_full_text_search.py
+++ databases/tests/test_full_text_search.py
@@ -19,14 +19,12 @@ class FullTextSearchSyntax(BaseModel):
 _mysql_syntax = FullTextSearchSyntax(
     search_or='cats dogs',
     search_and='+cats +dogs',
-    search_not='-dogs',
 )
 
 # Define the queries for PostgreSQL
 _postgresql_syntax = FullTextSearchSyntax(
     search_or='cats | dogs',
     search_and='cats & dogs',
-    search_not='!dogs',
 )
 
 # Map the syntax to the corresponding database
@@ -91,14 +89,3 @@ async def test_full_text_search(client: Prisma) -> None:
     assert len(posts) == 1
     assert 'cats' in posts[0].title
     assert 'dogs' in posts[0].title
-
-    # Test: Search for posts that contain 'cats' but not 'dogs'
-    posts = await client.post.find_many(
-        where={
-            'title': {
-                'search': syntax.search_not,
-            },
-        }
-    )
-    assert len(posts) == 2
-    assert 'dogs' not in posts[0].title
