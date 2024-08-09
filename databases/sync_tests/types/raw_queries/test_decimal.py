56dfa7cd98e36f1888a5ee36c40cb1565840c322
Astrea <25420078+AstreaTSS@users.noreply.github.com>
2024-08-09 05:42:41 -0400

    fix(raw queries): correct None handling for all fields (#1000)
    ## Change Summary
    
    See https://github.com/RobertCraigie/prisma-client-py/issues/998.
    
    ## Agreement
    
    By submitting this pull request, I confirm that you can use, modify,
    copy and redistribute this contribution, under the terms of your choice.
    
    ---------
    
    Co-authored-by: Robert Craigie <robert@craigie.dev>

--- databases/sync_tests/types/raw_queries/test_decimal.py
+++ databases/sync_tests/types/raw_queries/test_decimal.py
@@ -11,14 +11,17 @@ from ...._compat import LiteralString
 
 class Queries(BaseModel):
     select: LiteralString
+    select_null: LiteralString
 
 
 _mysql_queries = Queries(
     select='SELECT * FROM Types WHERE decimal_ = ?',
+    select_null='SELECT * FROM Types WHERE optional_decimal IS NULL',
 )
 
 _postgresql_queries = Queries(
     select='SELECT * FROM "Types" WHERE decimal_ = $1::numeric',
+    select_null='SELECT * FROM "Types" WHERE optional_decimal IS NULL',
 )
 
 RAW_QUERIES: DatabaseMapping[Queries] = {
@@ -26,6 +29,7 @@ RAW_QUERIES: DatabaseMapping[Queries] = {
     'mariadb': _mysql_queries,
     'sqlite': Queries(
         select='SELECT * FROM Types WHERE decimal_ = ?',
+        select_null='SELECT * FROM Types WHERE optional_decimal IS NULL',
     ),
     'postgresql': _postgresql_queries,
     'cockroachdb': _postgresql_queries,
@@ -55,3 +59,22 @@ def test_query_first(
     assert model is not None
     assert model.id == record2.id
     assert model.decimal_ == Decimal('1.24343336464224')
+
+
+def test_query_first_optional(
+    client: Prisma,
+    database: SupportedDatabase,
+) -> None:
+    """Use of decimal in raw SELECT queries with optional/nullable results"""
+    queries = RAW_QUERIES[database]
+
+    record = client.types.create({'optional_decimal': None})
+
+    found = client.query_first(queries.select_null)
+    assert found['id'] == record.id
+    assert found['optional_decimal'] is None
+
+    model = client.query_first(queries.select_null, model=Types)
+    assert model is not None
+    assert model.id == record.id
+    assert model.optional_decimal is None
