f08bc8bcea70d31a50ba43d857e70a1360238a12
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-11-18 19:24:02 +0100

    test(mongodb): create test for mongodb composite types

---
+++ databases/tests_mongodb/test_composite_types.py
@@ -0,0 +1,18 @@
+import pytest
+
+from prisma import Prisma
+
+
+@pytest.mark.asyncio
+async def test_composite_types(client: Prisma) -> None:
+    await client.user.create({
+        'name': 'Alice',
+        'contact': {
+            'email': 'test@test.com',
+            'phone': '123-456-7890'
+        }
+    })
+    user = await client.user.find_first()
+
+    assert user is not None
+    assert user.name == 'Alice'
--- databases/tests_mongodb/test_hello.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from prisma import Prisma
-
-
-@pytest.mark.asyncio
-async def test_base_usage(client: Prisma) -> None:
-    assert 'Hello, World!' == 'Hello, World!'
